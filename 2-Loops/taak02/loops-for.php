cdf8ba6d3d0dc1c170a01c14e12e00e646a04599
metin celik <metin.celik@student.rocva.nl>
2021-01-06 14:09:25 +0100

    loop for

---
+++ 2-Loops/taak02/loops-for.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php  
+$leeftijd = 0;
+
+for ($x = 0; $x <= 18; $x++) {
+    echo "Ik ben $x jaar, dus ik mag nog niet stemmen: $x <br>";
+  }
+  ?>  
+
+}
+?>  
+
+</body>
+</html>
